02506dbd2cea1e30caf8cbe380a99870f327e297
Athena Orchestration <athena@orion>
2026-05-23 19:22:18 +0000

    feat(substrate): add RepairEvidenceV1 + RepairPressureAppraisalV1 models

---
+++ orion/substrate/appraisal/__init__.py
@@ -0,0 +1,8 @@
+"""Substrate-derived appraisers. See docs/plans/substrate/2026-05-23-repair-pressure-v1.md."""
+
+from .models import RepairEvidenceV1, RepairPressureAppraisalV1
+
+__all__ = [
+    "RepairEvidenceV1",
+    "RepairPressureAppraisalV1",
+]

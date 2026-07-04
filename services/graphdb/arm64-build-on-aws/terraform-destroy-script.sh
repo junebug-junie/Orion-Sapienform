32f976e688df51f0225308ffedb7a06263f72469
Athena Orchestration <athena@orion>
2026-07-04 01:46:25 +0000

    removed dead services

--- services/graphdb/arm64-build-on-aws/terraform-destroy-script.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-echo 'GraphDB AWS env destruction started...'
-terraform_args=$1
-cd /opt/terraform-data/arm64-build-on-aws
-terraform destroy -auto-approve ${terraform_args}
-echo "Environment destroyed"
